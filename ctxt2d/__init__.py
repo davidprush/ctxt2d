4d74d355b8c6f125c95a8549dc22785fdf3373da
davidprush <davidprush@gmail.com>
2022-09-16 23:56:06 -0400

    Updates

--- ctxt2d/__init__.py
+++ ctxt2d/__init__.py
@@ -8,7 +8,7 @@ from importlib import metadata as importlib_metadata
 def get_version() -> str:
     try:
         return importlib_metadata.version(__name__)
-    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+    except importlib_metadata.PackageNotFoundError:
         return "unknown"
 
 
